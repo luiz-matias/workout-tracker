af11c8de2e7bae5d5b4007a57f896dd7adc7137b
Luiz Matias <luiz.matias1999@gmail.com>
2025-02-08 02:12:33 -0300

    feat: adjusted showing logs, queries and locale

--- src/main/kotlin/com/luizmatias/workout_tracker/config/exception/advice/ExceptionHandlerAdvice.kt
+++ src/main/kotlin/com/luizmatias/workout_tracker/config/exception/advice/ExceptionHandlerAdvice.kt
@@ -5,6 +5,8 @@ import com.luizmatias.workout_tracker.config.exception.common_exceptions.*
 import com.luizmatias.workout_tracker.dto.common.ErrorResponseDTO
 import jakarta.servlet.http.HttpServletRequest
 import org.springframework.http.HttpStatus
+import org.springframework.http.converter.HttpMessageConversionException
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
@@ -15,7 +17,10 @@ class ExceptionHandlerAdvice {
 
     @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(JWTCreationException::class)
-    fun handleJwtCreationException(exception: JWTCreationException, request: HttpServletRequest): ErrorResponseDTO {
+    fun handleJwtCreationException(
+        exception: JWTCreationException,
+        request: HttpServletRequest
+    ): ErrorResponseDTO {
         return ErrorResponseDTO(
             timestamp = Date(),
             status = HttpStatus.INTERNAL_SERVER_ERROR.value(),
@@ -52,6 +57,28 @@ class ExceptionHandlerAdvice {
         )
     }
 
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(
+        MethodArgumentNotValidException::class
+    )
+    fun handleInvalidArguments(
+        exception: MethodArgumentNotValidException,
+        request: HttpServletRequest
+    ): ErrorResponseDTO {
+        val errorMap: MutableMap<String, String?> = HashMap()
+        exception.bindingResult.fieldErrors.forEach {
+            errorMap[it.field] = it.defaultMessage
+        }
+
+        return ErrorResponseDTO(
+            timestamp = Date(),
+            status = HttpStatus.BAD_REQUEST.value(),
+            error = "Malformed data",
+            path = request.requestURI,
+            extras = errorMap
+        )
+    }
+
     @ResponseStatus(value = HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MalformedDataException::class)
     fun handleMalformedDataException(
@@ -61,7 +88,21 @@ class ExceptionHandlerAdvice {
         return ErrorResponseDTO(
             timestamp = Date(),
             status = HttpStatus.BAD_REQUEST.value(),
-            error = exception.message,
+            error = "Malformed data",
+            path = request.requestURI
+        )
+    }
+
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(HttpMessageConversionException::class)
+    fun handleHttpMessageConversionException(
+        exception: HttpMessageConversionException,
+        request: HttpServletRequest
+    ): ErrorResponseDTO {
+        return ErrorResponseDTO(
+            timestamp = Date(),
+            status = HttpStatus.BAD_REQUEST.value(),
+            error = "Malformed data",
             path = request.requestURI
         )
     }
