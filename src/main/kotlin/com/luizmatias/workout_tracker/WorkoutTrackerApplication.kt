b281d6add810d4dd2cbfa6f90390b450affe89b0
Luiz Matias <luiz.matias1999@gmail.com>
2025-02-09 01:56:47 -0300

    feat: force server timezone to UTC

--- src/main/kotlin/com/luizmatias/workout_tracker/WorkoutTrackerApplication.kt
+++ src/main/kotlin/com/luizmatias/workout_tracker/WorkoutTrackerApplication.kt
@@ -3,11 +3,13 @@ package com.luizmatias.workout_tracker
 import com.luizmatias.workout_tracker.db.seeder.DatabaseSeeder
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import java.util.*
 
 @SpringBootApplication
 class WorkoutTrackerApplication
 
 fun main(args: Array<String>) {
+    TimeZone.setDefault(TimeZone.getTimeZone("UTC"))
     val context = runApplication<WorkoutTrackerApplication>(*args)
     context.getBean(DatabaseSeeder::class.java).seed()
 }
