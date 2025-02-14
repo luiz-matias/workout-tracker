9865f8303adc554c054d3c610d46a69fd43a3e59
Luiz Matias <luiz.matias1999@gmail.com>
2025-02-13 21:35:51 -0300

    feat: sent failing test to verify CI

--- src/integrationTest/kotlin/com/luizmatias/workout_tracker/WorkoutTrackerApplicationIntegrationTests.kt
+++ src/integrationTest/kotlin/com/luizmatias/workout_tracker/WorkoutTrackerApplicationIntegrationTests.kt
@@ -3,6 +3,7 @@ package com.luizmatias.workout_tracker
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
+import org.springframework.util.Assert
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -12,6 +13,7 @@ class WorkoutTrackerApplicationIntegrationTests {
     fun contextLoads() {
         println("Context loaded successfully!")
         Thread.sleep(3000)
+        Assert.isTrue(1 == 2, "Context loaded successfully!")
     }
 
 }
