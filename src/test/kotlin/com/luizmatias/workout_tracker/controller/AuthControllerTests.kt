081deb037a7a537ff6d9cafcada7019f3288537c
Luiz Matias <luiz.matias1999@gmail.com>
2025-02-15 03:22:14 -0300

    fix: fixed spring boot test initialization

--- src/test/kotlin/com/luizmatias/workout_tracker/controller/AuthControllerTests.kt
+++ src/test/kotlin/com/luizmatias/workout_tracker/controller/AuthControllerTests.kt
@@ -21,6 +21,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
+import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.bean.override.mockito.MockitoBean
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
@@ -29,6 +30,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 class AuthControllerTests {
     @MockitoBean
     private lateinit var authService: AuthService
