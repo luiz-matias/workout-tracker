567f408e811aeb81232fb76cd8a9cdd153c63c66
Luiz Matias <luiz.matias1999@gmail.com>
2025-03-15 05:17:19 -0300

    docs: added docs for services

--- src/main/kotlin/com/luizmatias/workout_tracker/service/user/UserService.kt
+++ src/main/kotlin/com/luizmatias/workout_tracker/service/user/UserService.kt
@@ -2,28 +2,52 @@ package com.luizmatias.workout_tracker.service.user
 
 import com.luizmatias.workout_tracker.model.user.User
 
+/**
+ * Service for managing users. This service is accountable for managing CRUD operations related to users.
+ */
 interface UserService {
-    fun getUserByEmail(email: String): User?
+    /**
+     * Get a user by its email.
+     */
+    fun getUserByEmail(email: String): User
 
+    /**
+     * Register a new user.
+     */
     fun registerUser(user: User): User
 
+    /**
+     * Updates a user.
+     */
     fun updateUser(
         id: Long,
         user: User,
-    ): User?
+    ): User
 
+    /**
+     * Change the password of a user.
+     */
     fun changePassword(
         id: Long,
         existingPassword: String,
         newPassword: String,
     ): User
 
+    /**
+     * Reset the password of a user.
+     */
     fun resetPassword(
         token: String,
         newPassword: String,
     ): User
 
+    /**
+     * Verify the email of a user.
+     */
     fun verifyEmail(token: String): User
 
-    fun deleteUser(id: Long): Boolean
+    /**
+     * Delete a user.
+     */
+    fun deleteUser(id: Long)
 }
--- src/main/kotlin/com/luizmatias/workout_tracker/service/workout_log_post/WorkoutLogPostService.kt
+++ src/main/kotlin/com/luizmatias/workout_tracker/service/workout_log_post/WorkoutLogPostService.kt
@@ -4,21 +4,47 @@ import com.luizmatias.workout_tracker.model.user.User
 import com.luizmatias.workout_tracker.model.workout_log_post.WorkoutLogPost
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
+import com.luizmatias.workout_tracker.service.workout_log_group_post.WorkoutLogGroupPostService
 
+/**
+ * Service for managing workout log posts. This service is accountable for managing CRUD operations related to workout
+ * log posts.
+ *
+ * e.g. A user can create a workout log post, update it, delete it, and list all posts they have created by using this
+ * service.
+ *
+ * Note that this service only creates workout log posts linked to the user account. In order to share the workout into
+ * groups the user is part of, the user must use the [WorkoutLogGroupPostService].
+ */
 interface WorkoutLogPostService {
+    /**
+     * Get all workout log posts based on a user.
+     */
     fun getAllWorkoutLogPostsByUser(
         user: User,
         pageable: Pageable,
     ): Page<WorkoutLogPost>
 
-    fun getWorkoutLogPostById(id: Long): WorkoutLogPost?
+    /**
+     * Get a workout log post by its ID.
+     */
+    fun getWorkoutLogPostById(id: Long): WorkoutLogPost
 
+    /**
+     * Create a new workout log post.
+     */
     fun createWorkoutLogPost(workoutLogPost: WorkoutLogPost): WorkoutLogPost
 
+    /**
+     * Update a workout log post.
+     */
     fun updateWorkoutLogPost(
         id: Long,
         workoutLogPost: WorkoutLogPost,
-    ): WorkoutLogPost?
+    ): WorkoutLogPost
 
-    fun deleteWorkoutLogPost(id: Long): Boolean
+    /**
+     * Delete a workout log post.
+     */
+    fun deleteWorkoutLogPost(id: Long)
 }
